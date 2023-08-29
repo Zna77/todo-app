9365f1fc574b2a6d6d9736a4118e9bffaa4522b5
Mohamed <Zna77@proton.me>
2023-08-29 19:38:08 +0100

    Initial commit

---
+++ src/components/TodoForm.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function TodoForm() {
+  return (
+    <form className="">
+      <input type="text" className="" placeholder="What is the task today?" />
+      <button type="submit" className="" />
+    </form>
+  );
+}
